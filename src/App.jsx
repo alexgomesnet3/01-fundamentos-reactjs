880e2cb05b19cdc249da5eb0b2010119e54f402e
Alex Gomes <alex.gomes.net3@gmail.com>
2024-06-18 21:25:21 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Components/Header.jsx";
+import { Post } from "./Post.jsx";
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Post author="Alex" content="Primeiro Post publicado" />
+      <Post author="Felipe" content="Segundo Post publicado" />
+    </>
+  );
+}
+
+export default App;
